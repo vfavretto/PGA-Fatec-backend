4d645d99b81814d5f0a5af8c417f2746b419ea3d
Felipe <felipe6san@hotmail.com>
2025-05-02 17:46:24 -0300

    Fix(User): Correcao de create user usando dto

--- PGA-Backend/src/modules/user/services/create-user.service.ts
+++ PGA-Backend/src/modules/user/services/create-user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../user.repository';
 import { Prisma, Pessoa } from '@prisma/client';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class CreateUserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(data: Prisma.PessoaCreateInput): Promise<Pessoa> {
+  async execute(data: CreateUserDto): Promise<Pessoa> {
     return this.userRepository.create(data);
   }
 }
