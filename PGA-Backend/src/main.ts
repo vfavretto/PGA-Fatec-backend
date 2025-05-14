643353868ec9b40119efecd6f7dacdb5f6050ed2
Zan-Kir <murilo4santos@hotmail.com>
2025-05-14 15:04:30 -0300

    feat(Cors): Autorização CORS para o Frontend
    feat(.env): exemplo de .env

--- PGA-Backend/src/main.ts
+++ PGA-Backend/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
